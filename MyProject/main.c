6dfb6fd51df5abbddc1fee5fe874b6a6d2c45c2a
Ella Jameson <ellagjameson@gmail.com>
2024-06-08 04:11:58 -0700

    Initial commit

---
+++ MyProject/main.c
@@ -0,0 +1,18 @@
+#include <avr/io.h>      // Defines pins, ports, etc
+#include <util/delay.h>  // Functions to waste time
+
+#define LED_PIN PB1      // Must be a valid pin on PORTB
+#define DELAY_TIME 1000
+
+int main(void) {
+	DDRB |= (1<<LED_PIN);  // Set LED pin to output mode
+	
+	// Main Loop
+	while (1) {
+		PORTB |= (1<<LED_PIN);   // Turn LED on
+		_delay_ms(DELAY_TIME);   // Wait
+
+		PORTB &= ~(1<<LED_PIN);  // Turn LED off
+		_delay_ms(DELAY_TIME);   // Wait
+	}
+}
