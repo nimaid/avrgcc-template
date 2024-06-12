7686d4662f3458507f2eb489a8a40e092d789dfb
Ella Jameson <ellagjameson@gmail.com>
2024-06-12 02:06:09 -0700

    Added ATmega328p

--- MyProject/main.c
+++ MyProject/main.c
@@ -1,18 +1,23 @@
-#include <avr/io.h>      // Defines pins, ports, etc
-#include <util/delay.h>  // Functions to waste time
+#include <avr/io.h>
+#include <util/delay.h>
 
-#define LED_PIN PB1      // Must be a valid pin on PORTB
+#define LED_DDR     DDRB
+#define LED_PORT    PORTB
+#define LED_BIT     PB0
+
 #define DELAY_TIME 1000
 
 int main(void) {
-	DDRB |= (1<<LED_PIN);  // Set LED pin to output mode
+	LED_DDR |= (1<<LED_BIT);  // Set LED pin to output mode
 	
 	// Main Loop
 	while(1) {
-		PORTB |= (1<<LED_PIN);   // Turn LED on
+		LED_PORT |= (1<<LED_BIT);   // Turn LED on
 		_delay_ms(DELAY_TIME);   // Wait
 
-		PORTB &= ~(1<<LED_PIN);  // Turn LED off
+		LED_PORT &= ~(1<<LED_BIT);  // Turn LED off
 		_delay_ms(DELAY_TIME);   // Wait
 	}
+	
+	return 0;
 }
